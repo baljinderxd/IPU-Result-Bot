d80f7652e9c60b6424df28148786167b295e082c
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-06-27 22:49:28 +0530

    Port error corrected

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ app.post('/', function (req, res) {
     }
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log('Telegram app listening on port 3000!')
 })
